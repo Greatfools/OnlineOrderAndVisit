443bd682c871e7e7c2994212a50c1d5dce5bb9ac
shiyi <i_am_shiyi@163.com>
2016-11-24 14:32:41 +0800

    set up search

--- myProject/OrderAndVisit/urls.py
+++ myProject/OrderAndVisit/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name = 'index'),
+	url(r'^search', views.search, name = 'search'),
 ]
--- myProject/OrderAndVisit/views.py
+++ myProject/OrderAndVisit/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	return HttpResponse('hello world')
+
+def search(request):
+	if 'q' in request.GET and request.GET['q']:
+		q = request.GET['q'] # q is an object submitted by front
+	else:
+		return HttpResponse('Please submit a search term')
