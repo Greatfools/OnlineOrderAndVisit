651c6f235efe89652834c973b0d8d4799d524632
shiyi <i_am_shiyi@163.com>
2016-11-24 15:32:31 +0800

    setup login and logout

--- myProject/OrderAndVisit/urls.py
+++ myProject/OrderAndVisit/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name = 'index'),
 	url(r'^search', views.search, name = 'search'),
+
+	url(r'^ligin', views.login, name = 'login'),
+	url(r'^personIformation', views.personIformation, name = 'personInformation')
 ]
--- myProject/OrderAndVisit/views.py
+++ myProject/OrderAndVisit/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from .models import *
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 # Create your views here.
 def index(request):
 	return HttpResponse('hello world')
@@ -9,5 +10,36 @@ def index(request):
 def search(request):
 	if 'q' in request.GET and request.GET['q']:
 		q = request.GET['q'] # q is an object submitted by front
+		return HttpResponse('you success')
 	else:
 		return HttpResponse('Please submit a search term')
+
+def login(request):
+	errors = []
+	if 'name' in request.GET and request.GET['name']:
+		if 'password' in request.GET and request.GET['password']:
+			userName = request.GET['name']
+			userPassword = request.GET['password']
+			res = User.objects.filter(name = userName)
+			if not res:
+				errors.append('no such people')
+			else:
+				if res.password == userPassword:
+					request.session['member_id'] = res.id
+					return HttpResponseRedirect('/personInformation/?name=userName,password=userPassword')
+		else:
+			errors.append('please input your password')
+	else:
+		errors.append('please input your name')
+	if (errors):
+		return HttpResponseRedirect('/index/?errorMessage=errors')
+
+def logout(request):
+	try:
+		del request.session['member_id']
+	except KeyError:
+		pass
+	return HttpPesopnse("You're logged out")
+
+def personInformation(request):
+	
