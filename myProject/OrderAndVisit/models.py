59d87ed4ea1491d4dac71b2125737f29d2c95411
GreatBeast <zhuzhn999@163.com>
2016-11-24 23:44:35 +0800

    update v2.0 database

--- myProject/OrderAndVisit/models.py
+++ myProject/OrderAndVisit/models.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from __future__ import unicode_literals
 
 from django.db import models
@@ -5,9 +6,10 @@ from django.db import models
 # Create your models here.
 class Hospital(models.Model):
     name = models.CharField(max_length = 20)
-    level = models.CharField(max_length = 10)
     address =  models.CharField(max_length = 100)
+    img = models.CharField(max_length = 50)#存储为一个链接
     introduction = models.TextField(null = True)
+    phonenum = models.CharField(max_length = 20)
 
     def __unicode__(self):
         return self.name
@@ -17,6 +19,7 @@ class User(models.Model):
     password = models.CharField(max_length = 20)
     name = models.CharField(max_length = 20)
     sex = models.CharField(max_length = 4)
+    birthday = models.CharField(max_length = 20)
     idCard = models.CharField(max_length = 18)
     telephone = models.CharField(max_length = 11)
     creditLevel = models.IntegerField(default = 5)
@@ -27,6 +30,7 @@ class User(models.Model):
 class Department(models.Model):
     hospitalId = models.ForeignKey(Hospital)
     name = models.CharField(max_length = 20)
+    classinfo = models.CharField(max_length = 20)#科室大类
     introduction = models.TextField(null = True)
 
     def __unicode__(self):
@@ -37,6 +41,8 @@ class Doctor(models.Model):
     name = models.CharField(max_length = 10)
     title = models.CharField(max_length = 20)
     introduction = models.TextField(null = True)
+    address = models.CharField(max_length = 100)
+    fee = models.FloatField()#医生的挂号费
 
     def __unicode__(self):
         return self.name
@@ -51,9 +57,9 @@ class Admin(models.Model):
 class VisitMessage(models.Model):
     doctorId = models.ForeignKey(Doctor)
     visitDate = models.DateTimeField(auto_now_add = True)
-    visitTime = models.CharField(max_length = 16)
+    visitTime = models.CharField(max_length = 16)#早上/中午/晚上
     maxNumber = models.IntegerField(default = 0)
-    restNumber = models.IntegerField(default = 0)
+    restNumber = models.IntegerField(default = 0)#剩余可预约人数
 
     def __unicdoe__(self):
         return self.doctorId + self.visitDate
@@ -63,7 +69,7 @@ class OrderMessage(models.Model):
     visitId = models.ForeignKey(VisitMessage)
     orderTime = models.DateTimeField(auto_now_add=True)
     isPayed = models.BooleanField(default = False)
-    isCancel = models.BooleanField(default = False)
+    isCanceled = models.BooleanField(default = False)
 
     def __unicode__(self):
         return self.userId + self.visitId + self.orderTime
@@ -83,5 +89,5 @@ class OrderCancelMessage(models.Model):
 class PayMessage(models.Model):
     orderId = models.ForeignKey(OrderMessage)
     value = models.IntegerField()
-    Type = models.CharField(max_length = 10)
+    type = models.CharField(max_length = 10)
     status = models.CharField(max_length = 20)
