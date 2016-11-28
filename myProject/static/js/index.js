089704c29372bb87b6d411cef250e1c80ffa7301
Lynn <14211013@buaa.edu.cn>
2016-11-28 23:08:15 +0800

    add footer  & finish index basic
    same as header

--- myProject/static/js/index.js
+++ myProject/static/js/index.js
@@ -2,5 +2,8 @@
  * Created by 朱耀华 on 2016/11/28.
  */
 $(function () {
-    $('.header').load('head.html');
+    $('.header').load('header.html');
+})
+$(function () {
+    $('.footer').load('footer.html')
 })
