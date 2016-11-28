134f1dae38121e747722204e927167b47013454b
Lynn <14211013@buaa.edu.cn>
2016-11-28 21:51:53 +0800

    add header
    要使用header时，添加header块，js中添加如下
    $(function () {
    $('.header').load('head.html');
    })

--- myProject/static/js/head.js
+++ myProject/static/js/head.js
@@ -1,11 +1,12 @@
 /**
  * Created by Lynn on 2016/11/21.
  */
+
 $('.banner a').click(function () {
     var _this = $(this).parent();
     _this.siblings().removeClass('selected');
     _this.addClass('selected');
-});
+})
 
 $('.loginButton button').click(function () {
     $('.login-layer').removeClass('hidden');
---
+++ myProject/static/js/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by 朱耀华 on 2016/11/28.
+ */
+$(function () {
+    $('.header').load('head.html');
+})
