7f9244cc63e4f6b77aeff4b72e41cb30b3dd57af
“ru <ruifrombuaa@outlook.com>
2016-12-08 08:36:20 +0800

    temp vision for be
    need more css detail

--- myProject/static/js/index.js
+++ myProject/static/js/index.js
@@ -7,3 +7,24 @@ $(function () {
 $(function () {
     $('.footer').load('footer.html')
 })
+$('#navi-doctor').click(function () {
+    $('#promoted-box1').addClass('hidden');
+    $('#promoted-box2').addClass('hidden');
+    $('#promoted-box3').addClass('hidden');
+    $('#promoted-box4').removeClass('hidden');
+    $('#promoted-box5').removeClass('hidden');
+    $('#promoted-box6').removeClass('hidden');
+
+    $('#navi-doctor').addClass('active');
+    $('#navi-hosp').removeClass('active');
+})
+$('#navi-hosp').click(function () {
+    $('#promoted-box4').addClass('hidden');
+    $('#promoted-box5').addClass('hidden');
+    $('#promoted-box6').addClass('hidden');
+    $('#promoted-box1').removeClass('hidden');
+    $('#promoted-box2').removeClass('hidden');
+    $('#promoted-box3').removeClass('hidden');
+    $('#navi-doctor').removeClass('active');
+    $('#navi-hosp').addClass('active');
+})
